d27567921de62fb594e25f4dd45bfdc572ee7c0c
Elhiu2308 <elhiuchristian2308@gmail.com>
2024-07-17 23:21:01 -0600

    Guardia Nacional2

--- app/src/main/java/sesion05/postwork/Ejercicio08.kt
+++ app/src/main/java/sesion05/postwork/Ejercicio08.kt
@@ -1,2 +1,36 @@
 package sesion05.postwork
 
+/**FUNCIONES INLINE*/
+
+//inline fun saludar (nombre:String){
+//    println("Hola, $nombre")
+//}
+//fun main(){
+//    saludar("Maria")
+//}
+////fun main(){
+////    println("Hola Maria!")
+////}
+
+/** Crea una función inline que tome una lambda como parámetro y la ejecute dentro de un bloque try-catch.
+  Luego, usa esta función para ejecutar una operación que podría lanzar una excepción.
+ */
+
+inline fun ejecutarSeguro(operacion: () -> Unit) {
+    try {
+        operacion()
+    } catch (e: Exception) {
+        println("Se produjo una excepción: ${e.message}")
+    }
+}
+
+fun main() {
+    ejecutarSeguro {
+        val resultado = 10 / 0
+        println(resultado)
+    }
+
+    ejecutarSeguro {
+        println("Esta operación es segura")
+    }
+}
--- app/src/main/java/sesion05/postwork/Ejercicio09.kt
+++ app/src/main/java/sesion05/postwork/Ejercicio09.kt
@@ -1,25 +1,18 @@
 package sesion05.postwork
 
-/**FUNCIONES INLINE*/
-
-//inline fun saludar (nombre:String){
-//    println("Hola, $nombre")
-//}
-//fun main(){
-//    saludar("Maria")
-//}
-////fun main(){
-////    println("Hola Maria!")
-////}
+/**Crea una lista de números decimales que representen precios.
+ Utiliza reduce para calcular el total de los precios. Luego, usa forEach para imprimir cada precio con un formato específico
+ * (por ejemplo, con dos decimales y el símbolo de moneda).*/
 
 
-//
-//inline fun operacionMat(a: Int, b:Int, operacion: (Int, Int)->Int): Int{
-//    return operacion(a,b)
-//}
-//
-//fun main ()
-//val resultado = operacionMat(5,3){x,y -> x + y}
-//println(resultados)
-//
-//}
+fun main() {
+    val precios = listOf(19.99, 24.50, 9.95, 4.20, 99.99)
+
+    val total = precios.reduce { acc, precio -> acc + precio }
+
+    println("Total: ${"%.2f".format(total)}")
+
+    precios.forEach { precio ->
+        println("${"$%.2f".format(precio)}")
+    }
+}

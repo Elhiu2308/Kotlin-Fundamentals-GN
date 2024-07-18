6444b84a1b67964d3598a6f6071539eae6424c1f
Elhiu2308 <elhiuchristian2308@gmail.com>
2024-07-17 23:15:45 -0600

    Guardia Nacional2

---
+++ app/src/main/java/sesion05/postwork/Ejercicio09.kt
@@ -0,0 +1,25 @@
+package sesion05.postwork
+
+/**FUNCIONES INLINE*/
+
+//inline fun saludar (nombre:String){
+//    println("Hola, $nombre")
+//}
+//fun main(){
+//    saludar("Maria")
+//}
+////fun main(){
+////    println("Hola Maria!")
+////}
+
+
+//
+//inline fun operacionMat(a: Int, b:Int, operacion: (Int, Int)->Int): Int{
+//    return operacion(a,b)
+//}
+//
+//fun main ()
+//val resultado = operacionMat(5,3){x,y -> x + y}
+//println(resultados)
+//
+//}
